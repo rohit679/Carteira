c9cc733ad640ed6d70e688b9206f26bb5c96114b
Rohit <rohitprasad25061999@gmail.com>
2021-04-15 16:40:15 +0530

    :sparkles: added auth modules skeleton

---
+++ Backend/src/modules/auth/model/user_model.js
@@ -0,0 +1,19 @@
+import mongoose, { mongo } from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,     
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true  
+    },
+    password : {
+        type : String,
+        required : true
+    }
+});
+
+export default mongoose.model('users', userSchema);
