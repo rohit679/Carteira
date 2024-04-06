629f6de7a79524a8ee28baa00c092c4264a10364
Rohit Prasad <rohitprasad25061999@gmail.com>
2024-04-06 21:33:43 +0530

    database running success🎉

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,40 +1,43 @@
-import {finishApp, createAnApp} from './app';
-import "./modules/db/connection";
-import authModule from './modules/auth';
-import skillModule from './modules/skills';
-import experienceModule from './modules/experience';
-import educationModule from './modules/education';
-import projectModule from './modules/project';
-import linkModule from './modules/links';
-import personalInfoModule from './modules/personal-info';
-
-const PORT = 8080;
-
-const app = createAnApp();
-
-authModule.init(app);
-
-skillModule.init(app);
-
-experienceModule.init(app);
-
-educationModule.init(app);
-
-projectModule.init(app);
-
-linkModule.init(app);
-
-personalInfoModule.init(app);
-
-finishApp(app);
+import { config } from "dotenv";
+import { finishApp, createAnApp } from "./app";
+import { getSecret } from "./configuration.js";
+import { connectMongo } from "./utils/connect-db.js";
+// import authModule from "./modules/auth";
+// import skillModule from "./modules/skills";
+// import experienceModule from "./modules/experience";
+// import educationModule from "./modules/education";
+// import projectModule from "./modules/project";
+// import linkModule from "./modules/links";
+// import personalInfoModule from "./modules/personal-info";
 
 (async () => {
-    try {
-        await app.listen(PORT);
-        console.log("----Server Started----");
-    }
-    catch(err) {
-        console.log(err.message);
-        process.exit(1);
-    }
+  config({ path: ".env" });
+  connectMongo();
+
+  const secrets = getSecret();
+  const app = createAnApp();
+  const port = secrets.port || 3000;
+
+  app.get("/health-check", (req, res) => {
+    res.send("App is healthy 💚");
+  });
+
+//   authModule.init(app);
+//   skillModule.init(app);
+//   experienceModule.init(app);
+//   educationModule.init(app);
+//   projectModule.init(app);
+//   linkModule.init(app);
+//   personalInfoModule.init(app);
+
+  finishApp(app);
+
+  try {
+    await app.listen(port, () => {
+      console.log(`Yes, the app is up and running at ${port} 🎉`);
+    });
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
 })();
---
+++ Backend/src/utils/connect-db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { getSecret } from "../configuration.js";
+
+export const connectMongo = async () => {
+  try {
+    const mongoUrl = getSecret().mongoUrl;
+    await mongoose.connect(mongoUrl, {
+      dbName: 'carteira',
+      useUnifiedTopology: true
+    });
+    console.log("Database connected successfully ✨");
+  } catch (err) {
+    console.error("Database connection error 💩");
+  }
+}
